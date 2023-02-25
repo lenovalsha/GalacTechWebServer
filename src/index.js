03c04bc72599e33c9131d7a7d96e5c4dc0088061
lenovalsha <lenovalsha@gmail.com>
2023-02-24 17:57:24 -0700

    should be good now

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
 import App from "./Profile"
 import Navigation from "./Navigation"
 import Contact from "./Contact"
 import Services from "./Services"
 import Slider from "./Slider"
+import "./styles.css"
 
-ReactDOM.render(<Slider/>,document.getElementById("instruction"));
-ReactDOM.render(<App/>,document.getElementById("team"));
-ReactDOM.render(<Navigation/>,document.getElementById("nav"));
-ReactDOM.render(<Contact/>,document.getElementById("contact"));
-ReactDOM.render(<Services/>,document.getElementById("service"));
+ReactDOM.render(<Slider />, document.getElementById("instruction"));
+ReactDOM.render(<App />, document.getElementById("team"));
+ReactDOM.render(<Navigation />, document.getElementById("nav"));
+ReactDOM.render(<Contact />, document.getElementById("contact"));
+ReactDOM.render(<Services />, document.getElementById("service"));
 
 
 
